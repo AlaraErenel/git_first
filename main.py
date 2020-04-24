7e90d38fb3c2a1406b0920fff491317ec17eb692
Alara ERENEL <alara_erenel@hotmail.com>
2020-04-24 19:27:08 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#from email_receiver import getEmail
+#from email_sender import postEmail
+
+def getEmail():
+    #...
+    
+    print("Email received...")
+
+def postEmail():
+    #...
+    
+    print("Email send...")
+
+def main():
+    print("1 - Inbox")
+    print("2 - New")
+    q = input("Seçim = ")
+    if q ==1:
+        getEmail()
+    elif q ==2:
+        postEmail()
+    else:
+        print("Yanlış tercih")
